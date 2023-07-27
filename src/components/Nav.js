79eab3fa8a3c36fa752301f6d0edd386a9635d5a
krishnapriyaks400 <krishnapriyaks400@gmail.com>
2023-07-28 01:25:21 +0530

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<a class="navbar-brand" href="budlogo.png">
+<img src="budlogo.png" alt="logo"/></a>
+
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="Home.js">HOME</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">KNOW US</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">SERVICES</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">CLIENTS</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">DIGITAL MARKETING</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">SOCIAL MEDIA</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">FILMS</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">CAREER</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">CONTACTS</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">+919937256621</a>
+    </li>
+    </ul>
+   </nav>
+
+ 
+  )
+}
+
+export default Nav
---
+++ src/pages/Home.js
@@ -0,0 +1,335 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+     <img class="pic" src="email.jpg" alt="homepic"></img>
+    
+     <div class="container">
+    <div class="row">
+    <div class="col-sm-8" >
+        <h2>ARE YOU HUNTING FOR A GOOD MARKETING STRATEGY  
+TO ENGAGE WITH YOUR CLIENTS ACTIVELY?</h2>
+<br/><p class="Email" >Email marketing campaigns are the best marketing solutions for you</p>
+<br/><p class="optimal" >Optimal email marketing campaigns inspire excitement and curiosity. It creates
+meaningful relationships through wonderfully crafted words. Prominent email
+marketing companies in India, like BUD, can help you personalize your story. In
+other words, we deliver tailored messages straight to your client & inbox. Our email
+campaigns convey your company & goals and targets. We resonate with the
+recipients, their needs, etc., capturing their attention and inspiring them to act.</p>
+<br/>
+<button type="button" class="btn btn-outline-danger">Read More</button>
+</div>
+
+    <div class="col-sm-4" >
+    <form class="needs-validation" novalidate>
+    <div class="form-group">
+      
+        <h3 class="get">GET IN TOUCH WITH US</h3>
+        <br/>
+      <input type="text" class="form-control" id="uname" placeholder="Full name" name="uname" required></input>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+
+      <br/>
+      <input type="text" class="form-control" id="uname" placeholder="Email Address" name="uname" required></input>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+
+      <br/>
+      <input type="text" class="form-control" id="uname" placeholder="Phone number" name="uname" required></input>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+
+      <br/>
+      <input type="text" class="form-control" id="uname" placeholder="Company name" name="uname" required></input>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+
+      <br/>
+      <input type="text" class="form-control" id="uname" placeholder="Website URL" name="uname" required></input>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+<br/>
+      <button type="submit" class="btn btn-primary">Request Free Audit</button>
+    </div>
+    </form>
+    
+    </div>
+  </div>
+  </div>
+  <br/>
+  <div class="container-fluid">
+    <div class="row">
+    <div class="col-sm-4" >
+    <img class="img-fluid" src="Group 1621.png" alt="homepic2"></img>
+    </div>
+    <div class="col-sm-8" >
+      <h2 class="if" >IF SOCIAL MEDIA IS FOR REACH, EMAIL MARKETING IS 
+FOR REVENUE</h2>
+<br/>
+<p class="imagine">Imagine a dynamic marketing channel that opens a direct line of
+communication between businesses and their valued customers! This is
+possible through strategic email marketing campaigns.<br/>
+<br/> 
+Bud & email campaigns are highly creative & amp; vibrant channels. We help
+you share exciting product launches, exclusive sales offers, and compelling
+news with your targeted audience. As a leading email marketing
+agency, coupled with our strategic approach, we deliver remarkable ROI.<br/>
+<br/>
+Gone are the days when mass mailings were sent out in a scattergun fashion.
+Modern email marketing strategy uses the power of consent, segmentation,
+</p>
+      </div>
+    </div>
+    </div>
+  <br/>
+  <br/>
+  <div class="mask1">
+    <p class="quality">
+  QUALITY VS QUANTITY, WE TRADE OFF THE RIGHT WAY!
+  </p>
+  </div>
+  <br/>
+  <div class="row">
+    <div class="col" >Bud's promotional emails are magic wands for
+spreading the right word about your product or
+service. They can also communicate special
+offers, limited-time deals, or exclusive content.
+Bud plans email designs in an appetizing way to
+ignite curiosity, create desire, and build bonds
+with your targeted audience. For a successful
+email marketing strategy, the frequency of
+emails needs to be increased to reinforce the
+message. According to the
+studies, 61% of email subscribers prefer weekly
+promotional marketing emails</div>
+    <div class="col" >Our informational emails help your targeted
+audience gain deeper insights into your
+business or brand. Its main aim is to offer
+valuable information enriching the receiver&
+knowledge. So consistency is the key here.
+Bud being a top email marketing company
+in India, we plan your email designs so that
+your target audiences don't & get bored.</div>
+    <div class="col" >Re-engagement emails are yet another
+important type of email marketing. As the
+name implies, these emails assist in
+reconnecting with customers or subscribers
+who haven't & been active in a while. Bud
+plans these email campaigns & times them
+for the inactive customers to get back to you.
+Our proficient email marketing
+services deliver the best results from
+your email campaigns.
+</div>
+  </div>
+  <br/>
+  <h2 class="we">
+  WE DELIVER THE RIGHT MESSAGE, TO THE RIGHT AUDIENCE, AT THE RIGHT TIME
+  </h2>
+  <p class="budplans">Bud plans your email marketing campaigns to offer many advantages benefitting your
+business. Studies state that around 83% of consumers engage better through effective email
+campaigns. Our holistic email marketing services assure the following benefits:</p>
+
+
+<img class="ss" src="ss.png" alt="homepic10"></img>
+<br/>
+
+
+<p class="bud2">Bud, a top email marketing company, can enhance customer loyalty and retention. We foster relationships through
+cost-effective email marketing campaigns. We help you achieve greater mind share among our targeted audience. We
+stimulate timely, regular communications with existing customers, encouraging repeat purchases.</p>
+    <br/>
+    <div class="container-fluid">
+      <h2 class="dear">DEAR USERS, FIND OUT WHAT MAKES US DIFFERENT</h2>
+      <p class="webelieve">WE BELIEVE THAT PERSONALIZATION IS NOT ABOUT FIRST/LAST NAMES. IT’S ABOUT RELEVANT CONTENT:</p>
+    <p class="onthe" >On the surface, email marketing seems to be an easy task - crafting an attractive mailer and sending them out to as many contacts
+as possible. But when there are a variety of companies across a variety of industries doing the same, content becomes critical.
+Content represents all forms of media including graphics and verbal communication. It needs to be relevant, fresh, and consistent,
+to do so, market research is necessary. It is important to understand fundamental questions like ‘What fits?’,
+‘Where?’ etc.
+That’s where the proficiency of Bud, the top email marketing company in India comes into play. We can deliver exceptional
+results to drive your business forward.</p>
+    </div>
+    <br/>
+
+
+    <div class="row">
+    <div class="col" >
+    <img id="task" src="task.png" width="1" height="1"></img>
+    <h3 class="taskwe">We plan & strategise</h3>
+    <p class="tasksurf">On the surface, email marketing seems
+to be an easy task - crafting an
+attractive mailer and sending them out
+to as many contacts as possible. But
+when there are a variety of companies
+across a variety of industries doing the
+same, content becomes . 
+We plan & strategise
+<b>Read more..</b>
+</p>
+    </div>
+    <div class="col" >
+    <img id="illustration" src="illustration.png" width="1" height="1"></img>
+    <h3 class="taskwe">We don’t just deliver, we design</h3>
+    <p class="tasksurf">Good email campaigning begins
+with exceptional email design at
+Bud email marketing
+company in India. Our team of
+skilled designers crafts captivating
+and immersive email
+templates. So you can effortlessly captivate attention and fue
+<b>Read more..</b>
+</p>
+    </div>
+    <div class="col" >
+    <img id="engineering" src="ngineering.png" width="1" height="1"></img>
+    <h3 class="taskwe">We are abreast of automation</h3>
+    <p class="tasksurf">To optimize the potency of your email
+campaigns, we harness the power of
+cutting-edge marketing
+automation tools. These tools
+empower us to streamline many
+facets of your email
+marketing journey. This includes
+audience segmentation,
+<b>Read more..</b></p>
+    </div>
+    <div class="col" >
+    <img id="funding" src="funding.png" width="1" height="1"></img> 
+    <h3 class="taskwe">We stay light on your pocket</h3>
+    <p class="tasksurf">Being India's best email marketing
+company, we know the significance of
+cost-effectiveness. We provide flexible
+pricing choices tailored to businesses of
+all scales. We guarantee optimal value
+for your investment. Our dedicated
+team can deliver top-notch
+<b>Read more..</b>
+</p>
+    </div>
+    </div>
+    <br/>
+    <h2 class="personalization">WE BELIEVE THAT PERSONALIZATION IS NOT ABOUT FIRST/LAST NAMES. IT’S ABOUT RELEVANT CONTENT</h2>
+    <p class="person">On the surface, email marketing seems to be an easy task - crafting an attractive mailer and sending them out to as many contacts
+as possible. But when there are a variety of companies across a variety of industries doing the same, content becomes critical.
+Content represents all forms of media including graphics and verbal communication. It needs to be relevant, fresh, and consistent,
+to do so, market research is necessary. It is important to understand fundamental questions like ‘What fits?’
+,
+‘Where?’ etc.
+That’s where the proficiency of Bud, the top email marketing company in India comes into play. We can deliver exceptional
+results to drive your business forward.</p>
+<br/>
+
+<div class="container-fluid-col">
+    <div class="row">
+    <div class="col-sm-5" >
+    <img class="img-fluid" src="Group 1627.png" alt="homepic3"></img>
+    </div>
+    <div class="col-sm-7" >
+      <h2 class="if-if" > OUR CUTTING-EDGE EMAIL MARKETING CAMPAIGNS</h2>
+  <h4 class="well">Welcome Email Series</h4>
+  <div class="pagination">
+  <a href="#">❮</a> 
+  <p class="create">To create a stellar first impression for your new subscribers, Bud offers the
+welcome email series. The email campaign design involves sending a series of
+emails. These emails introduce your brand, give a warm welcome, and deliver
+valuable content. It is also used for enticing incentives to create a bang
+opening. Bud can help you captivate your targeted audience through this
+email marketing strategy. Bring your brand to life through our
+expert email marketing services.
+</p>
+  <a href="#">❯</a>
+</div>
+
+</div>
+</div>
+</div>
+<br/>
+
+<h2 class="opt">OPTIMIZE YOUR CAMPAIGNS WITH BUD'S EMAIL MARKETING
+STRATEGIES</h2>
+< div class="row">
+
+<div class="col-2">Building a Quality
+Email List</div>
+
+
+<div class="col-3">To create a stellar first impression for your new
+subscribers, Bud offers the welcome email series.
+The email campaign design involves sending a
+series of emails. These emails introduce your brand,
+give a warm welcome, and deliver valuable content.
+It is also used for enticing incentives to create a
+bang opening. Bud can help you captivate
+your targeted audience through this email
+marketing strategy. Bring your brand to life through
+our expert email marketing services.</div>
+
+<div class="col-2">Crafting Engaging
+Subject Lines</div>
+
+</div>
+    <br/>
+    < div class="row">
+    <div class="col-2">Delivering High-Quality
+Content</div>
+
+
+<div class="col-2">Optimizing for
+Mobile Devices</div>
+
+
+<div class="col-2">Compelling 
+Call-to-Action (CTA)</div>
+</div>
+    <br/>
+    < div class="row">
+    <div class="col-2">Experimenting with
+A/B Testing
+</div>
+
+<div class="col-2">Leveraging Automation
+& Drip Campaigns</div>
+
+
+<div class="col-2">Monitoring &
+Analysing Performance</div>
+     </div>
+    <br/>
+    <p class="long">Get ready for a world of infinite possibilities. Unleash the potential of emailmarketing with India
+'
+s best email marketing company. We
+tap into tech-charged minds with creative strategists. That'
+s how we have evolved as an innovative digital marketing
+agency in Bangalore. Email marketing is one of the many automation tools vital for a holistic marketing campaign design.<br/> We
+understand the importance of what to say and how to say it!
+Onboard Bud is India
+'
+s best email marketing company to experience data-drivendecision-making. Boost your
+email marketing automation efforts throughstrategic campaign design & implementation. Unlock smart email marketing
+automation tools for cost-effective solutions. Give your brand wings to soar high in the online & offline world.
+ Bud is one of the top email marketing agencies because we integrate social media marketing, SEO, PPC Advertising, content & email
+marketing. Outsmart competition with our 360-degree branding services &; advertising campaigns and enjoy assured ROI.
+</p>
+<br/>
+<div class="container-fluid-flu">
+  <h3 class="last">Is something you want to discuss with us? </h3>
+  <button type="button" class="btn btn-primary">Leave a message</button>
+  <div class="row">
+    <div class="col-co" ><h5>Brand Management</h5></div>
+    <div class="col-co" > <h5>Creative servive</h5></div>
+    <div class="col-co" > <h5>digital marketing</h5></div>
+    <div class="col-co" ><h5>Get in touch with us</h5></div>
+    </div>
+
+</div>
+<footer>
+  <p class="copy">copyright budindia2023</p>
+</footer>
+    </>
+  )
+}
+
+export default Home
